da31bc51f2e862834046876d278b566addad898c
Electrizard <rollingskyisawsome@gmail.com>
2025-07-13 03:23:22 -0400

    css changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import {CartContext} from './contexts/CartContext.jsx';
 import CashDisplay from "./CashDisplay.jsx";
 import Store from "./Store.jsx";
 function App() {
-  const [cash, setCash] = useState(1000000);
+  const [cash, setCash] = useState(10000000);
   const [cart, setCart] = useState([]);
   return(
     <CashContext.Provider value={{cash, setCash}}>
