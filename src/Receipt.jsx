f15811b98aea7af26f776932240328bab2fc1439
Electrizard <rollingskyisawsome@gmail.com>
2025-07-15 02:33:19 -0400

    change receipt text

--- src/Receipt.jsx
+++ src/Receipt.jsx
@@ -9,10 +9,10 @@ function Receipt(){
         <div className="receipt-container">
             <h1>Receipt: </h1>
             {cart.map((cartItem) => {
-                return(<p>{cartItem.item.name}: ${cartItem.item.price} - x{cartItem.quantity}</p>)
+                return(<p>{cartItem.item.name}: ${cartItem.item.price.toLocaleString()} - x{cartItem.quantity}</p>)
             })}
             <hr></hr>
-            <h2>Total: {cart.reduce((sum, cartItem) => (sum + cartItem.item.price * cartItem.quantity), 0).toLocaleString()}</h2>
+            <h2>Total: ${cart.reduce((sum, cartItem) => (sum + cartItem.item.price * cartItem.quantity), 0).toLocaleString()}</h2>
         </div>
     );
 }  
